f79007f36ef07c2ad62135339dd3ab39aba58f2b
Perishte <javascript_9595@bk.ru>
2023-11-28 22:54:14 +0500

    added search functionality with useParams

--- src/components/BlogPage/index.jsx
+++ src/components/BlogPage/index.jsx
@@ -1,45 +1,43 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import {Link, useSearchParams, useLocation} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
+import { BlogFilter } from '../BlogFilter/index';
 
-//useLocation object of location
-//private routes are needed to use wrap some component and to make it private f.e for authorization
-function BlogPage() {
+const BlogPage = () => {
   const [posts, setPosts] = useState([]);
+  console.log(useLocation());
   const [searchParams, setSearchParams] = useSearchParams();
-  const postQuery = searchParams.get('post')  || '';
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const query = form.search.value;
-    setSearchParams({post: query});
-  }
+
+  const postQuery = searchParams.get('post') || '';
+  const latest = searchParams.has('latest');
+
+  const startsFrom = latest ? 80 : 1;
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
-      .then(data => setPosts(data));
+      .then(data => setPosts(data))
   }, []);
-  console.log(useLocation());
+  return (
+    <div>
+      <h1>Our news</h1>
+      <Link to="/posts/new">Add new post</Link>
 
-  return(
-    <>
-      <h1>Blog Page</h1>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-         <input type="search" name="search" />
-         <input type="submit" value="Search" />
-      </form>
-      <Link to='/posts/new'>Add new post</Link>
+      <BlogFilter postQuery={postQuery} latest={latest} setSearchParams={setSearchParams} />
+
+      <Link to="/posts/new" style={{margin: '1rem 0', display: 'inline-block'}}>Add new post</Link>
       {
-        posts
-          .filter(post => post.title.includes(postQuery))
-          .map(post => (
-        <Link key={post.id} to={`/posts/${post.id}`}>
-          <li>{post.title}</li>
-        </Link>
-        ))
+        posts.map(post => (
+          posts.filter(
+            post => post.title.includes(postQuery) && post.id >= startsFrom
+          ).map(post => (
+            <Link key={post.id} to={`/posts/${post.id}`}>
+              <li>{post.title}</li>
+            </Link>
+          ))
+        ))};
       }
-    </>
+    </div>
   )
-};
-
+}
 export default BlogPage;
